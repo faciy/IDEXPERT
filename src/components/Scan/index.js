2a7bfc983c071fa1996ce59bbc7cecae6b3eeca6
KOUASSI CHARLES OTH-NIEL <kouassicharlesothniel@gmail.com>
2021-03-23 13:26:26 +0000

    add scan

--- src/components/Scan/index.js
+++ src/components/Scan/index.js
@@ -1,45 +1,131 @@
-import React from 'react'
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState,useEffect,useRef } from 'react'
+import { 
+    Text,
+    View,
+    TouchableOpacity,
+    Image,
+    Linking } from 'react-native';
 import Navbar from '../common/Navbar';
 import styles from './styles';
 import imageLogo from '../../assets/icons/logoIdExpert.png';
 import Menu from '../../assets/icons/menu.png';
-import scan from '../../assets/images/scan.png';
+// import scan from '../../assets/images/scan.png';
 import { useNavigation } from '@react-navigation/native';
 import { DrawerActions } from '@react-navigation/native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import check from '../../assets/icons/check.png';
+import ButtonCustom from '../common/ButtonCustom';
+// import { RNCamera } from 'react-native-camera';
 
-const ScanComponent = () => {
+
+const  ScanComponent = () => {
+    const scanner = useRef(null);
+    const [scan, setScan] = useState(false);
+    const [result, setResult] = useState(null)
+
+    useEffect(() => {
+        setResult(null)
+    }, [])
+
+    const onSuccess = e => {
+        // Linking.openURL(e.data).catch(err =>
+        //   console.error('An error occured', err)
+        // );
+        setResult(e)
+        setScan(true)
+        if(e.data.substring === 'http'){
+            alert(e.data)
+        }
+
+      };
 
     const navigation = useNavigation();
+        return scan ? (
+            <View style={styles.other}>
+                 <View style={styles.header}>
+                    <Navbar
+                        imageLogo={<Image 
+                        style={styles.img}    
+                        source={imageLogo} />}
+                        imageOther={
+                            <TouchableOpacity
+                            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+                            >
+                                 <Image 
+                                    style={styles.menu}
+                                    source={Menu} />
+                            </TouchableOpacity>}
+                    />
+                    <View style={{marginTop:50}}>
+                        <Text style={styles.text} >SCAN</Text>
+                    </View>
+                </View>
 
-    return (
-        <View>
-            <View style={styles.header}>
-                <Navbar
-                    imageLogo={<Image 
-                    style={styles.img}    
-                    source={imageLogo} />}
-                    imageOther={
-                        <TouchableOpacity
-                        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-                        >
-                             <Image 
-                                style={styles.menu}
-                                source={Menu} />
-                        </TouchableOpacity>}
-                />
-                <View style={{marginTop:50}}>
-                    <Text style={styles.text} >Nom du cabinet</Text>
+                <View style={styles.correct}>
+                    <View style={styles.ContainerCorrect}>
+                        <View style={styles.iconValidate}>
+                            <Image 
+                            style={{
+                                width:50,
+                                height:50,
+                                tintColor:'green',
+                            }}
+                            source={check}
+                            />
+                        </View>
+                        <View style={styles.textCorrect}>
+                            <Text style={{color:'white'}}>Le document que vous avez scanné a bien éte édité par le cabinet</Text>
+                        </View>
+                        <View >
+                            <TouchableOpacity
+                            onPress={() => {}}
+                            style={styles.btn}
+                            >
+                                <Text style={styles.textRenvoyer}>Renvoyer</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity
+                            onPress={() => {}}
+                            style={styles.btn}
+                            >
+                                <Text style={styles.textRenvoyer}>Valider</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
             </View>
-            {/* code scan  */}
-            <View style={styles.scan} >
-                <Image 
-                source={scan}
-                />
-            </View>
-        </View>
-    )
+
+        ) : (
+            <View>
+                <View style={styles.header}>
+                    <Navbar
+                        imageLogo={<Image 
+                        style={styles.img}    
+                        source={imageLogo} />}
+                        imageOther={
+                            <TouchableOpacity
+                            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+                            >
+                                 <Image 
+                                    style={styles.menu}
+                                    source={Menu} />
+                            </TouchableOpacity>}
+                    />
+                    <View style={{marginTop:50}}>
+                        <Text style={styles.text} >Nom du cabinet</Text>
+                    </View>
+                </View>
+                {/* code scan  */}
+                    <View style={styles.scan} >
+                        <QRCodeScanner
+                        onRead={onSuccess}
+                        ref={scanner}
+                        showMarker={true}
+                        reactivate={true}
+                        />
+                    </View>
+                </View>
+        )
 }
 
 export default ScanComponent;
--- src/components/Scan/styles.js
+++ src/components/Scan/styles.js
@@ -1,32 +1,95 @@
 import {
-    StyleSheet
+  StyleSheet
 } from 'react-native';
 import { SIZES } from '../../assets/theme';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
-    header: {
-        backgroundColor: '#1F4F4F',
-        width: SIZES.width,
-        height: SIZES.height - 550,
-        paddingHorizontal:20
-    },
-    menu: {
-        width: 30,
-        height: 30,
-        tintColor:'white'
-    },
-    text:{
-        color: 'white',
-        textAlign:'center' 
-    },
-    scan:{
-        justifyContent:'center',
-        alignItems:'center',
-        marginTop:100
-    },
-    img:{
-        width:40,
-        height:40,
-        alignSelf:'center'
-    }
+  header: {
+    backgroundColor: '#1F4F4F',
+    width: SIZES.width,
+    height: SIZES.height - 550,
+    paddingHorizontal: 20
+  },
+  menu: {
+    width: 30,
+    height: 30,
+    tintColor: 'white'
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  textCorrect: {
+    padding: wp('5%')
+  },
+  scan: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: hp('8%')
+  },
+  img: {
+    width: 40,
+    height: 40,
+    alignSelf: 'center'
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  },
+  other: {
+    //   flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  correct: {
+    height: hp('100%'),
+    width: wp('100%'),
+    backgroundColor: '#215D5D'
+  },
+  ContainerCorrect: {
+    width: wp('80%'),
+    height: hp('60%'),
+    margin: hp('5%'),
+    borderColor: 'grey',
+    borderWidth: 2,
+    backgroundColor: '#1F4F4F',
+    alignItems: 'center'
+  },
+  iconValidate: {
+    width: wp('30%'),
+    height: hp('15%'),
+    backgroundColor: 'white',
+    borderRadius: wp('50%'),
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btn: {
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingEnd: wp('50%'),
+    backgroundColor: '#127171',
+    borderRadius: 30,
+    height: 50,
+  },
+  textRenvoyer: {
+    color:'white',
+    left:wp('25%')
+  }
 })
